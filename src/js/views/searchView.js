d30d79410663ae40685a1bffcd17b0bd93bcabc2
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-03-21 16:54:33 +0500

    feat: extend SearchVIew with VIew superclass

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,21 +1,22 @@
-class SearchView {
-  #parentElement = document.querySelector('.search');
+import View from './View.js';
+class SearchView extends View {
+  _parentElement = document.querySelector('.search');
 
   getQuery() {
-    const query = this.#parentElement.querySelector('.search__field').value;
+    const query = this._parentElement.querySelector('.search__field').value;
     this.#clearInput();
     return query;
   }
 
   addHandlerSearch(handler) {
-    this.#parentElement.addEventListener('submit', function (e) {
+    this._parentElement.addEventListener('submit', function (e) {
       e.preventDefault();
       handler();
     });
   }
 
   #clearInput() {
-    this.#parentElement.querySelector('.search__field').value = '';
+    this._parentElement.querySelector('.search__field').value = '';
   }
 }
 
