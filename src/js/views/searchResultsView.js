be2afa5a71783d330f4671493adb777e3b53841b
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-03-21 18:00:45 +0500

    feat: method to genereate previews from search results

--- src/js/views/searchResultsView.js
+++ src/js/views/searchResultsView.js
@@ -3,6 +3,31 @@ import View from './View.js';
 class SearchResultsView extends View {
   _parentElement = document.querySelector('.results');
   _errorMessage = `No recipes found for your query! Please try again.`;
+
+  _generateMarkup() {
+    this._data.map(this.#generateMarkupPreviews).join('');
+  }
+
+  #generateMarkupPreviews() {
+    return `
+        <li class="preview">
+          <a class="preview__link preview__link--active" href="#23456">
+            <figure class="preview__fig">
+              <img src="src/img/test-1.jpg" alt="Test" />
+            </figure>
+            <div class="preview__data">
+              <h4 class="preview__title">Pasta with Tomato Cream ...</h4>
+              <p class="preview__publisher">The Pioneer Woman</p>
+              <div class="preview__user-generated">
+                <svg>
+                  <use href="src/img/icons.svg#icon-user"></use>
+                </svg>
+              </div>
+            </div>
+          </a>
+        </li>
+    `;
+  }
 }
 
 export default new SearchResultsView();
