249229bb966c6e0d8cadce053a3dbb1d9c7f9dd0
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-03-21 17:00:59 +0500

    feat: add properties to searchResultsView

--- src/js/views/searchResultsView.js
+++ src/js/views/searchResultsView.js
@@ -1,5 +1,8 @@
 import View from './View.js';
 
-class SearchResultsView extends View {}
+class SearchResultsView extends View {
+  _parentElement = document.querySelector('.search-results');
+  _errorMessage = `No recipes found for your query! Please try again.`;
+}
 
 export default new SearchResultsView();
