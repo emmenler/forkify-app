36cf932b94098a9a4644d7b35aeeb395011d7a8c
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-03-22 20:13:11 +0500

    feat: add bookmarksView.js

---
+++ src/js/views/bookmarksView.js
@@ -0,0 +1,36 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+
+class BookmarksView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = `No recipes found for your query! Please try again.`;
+
+  _generateMarkup() {
+    return this._data
+      .map((result) => {
+        return this.#generateMarkupPreviews(result);
+      })
+      .join('');
+  }
+
+  #generateMarkupPreviews(result) {
+    const id = window.location.hash.slice(1);
+    return `
+        <li class="preview">
+          <a class="preview__link ${
+            result.id === id ? 'preview__link--active' : ''
+          }" href="#${result.id}">
+            <figure class="preview__fig">
+              <img src="${result.image}" alt="${result.title}" />
+            </figure>
+            <div class="preview__data">
+              <h4 class="preview__title">${result.title}</h4>
+              <p class="preview__publisher">${result.publisher}</p>
+            </div>
+          </a>
+        </li>
+    `;
+  }
+}
+
+export default new BookmarksView();
