c621bb763c25c6016246293ff3fb0fc93b50ad6c
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-03-22 10:56:11 +0500

    feat: markup for dynamic rendering pagination buttons

--- src/js/views/searchPaginationView.js
+++ src/js/views/searchPaginationView.js
@@ -1,4 +1,6 @@
 import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+
 class SearchPaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
@@ -6,21 +8,49 @@ class SearchPaginationView extends View {
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
-    console.log(numPages, this._data.page);
+    const curPage = this._data.page;
+    console.log(numPages, curPage);
     // 1. First page, only button to move forward
-    if (this._data.page === 1 && numPages > 1) {
-      return 'first page, with others';
+    if (curPage === 1 && numPages > 1) {
+      return `
+          <button class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+          </button>
+      `;
     }
     // 2. Last page, only button to move backward
-    if (this._data.page === numPages && numPages > 1) {
-      return 'last page, with others';
+    if (curPage === numPages && numPages > 1) {
+      return `
+          <button class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${curPage - 1}</span>
+          </button>
+      `;
     }
     // 3. Both buttons are present
-    if (this._data.page < numPages) {
-      return 'other page';
+    if (curPage < numPages) {
+      return `
+          <button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${curPage - 1}</span>
+          </button>
+          <button class="btn--inline pagination__btn--next">
+            <span>Page ${curPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+          </button>
+      `;
     }
     // 4. Less that RES_PER_PAGE previews on page, no buttons
-    return 'only one page';
+    return '';
   }
 }
 
