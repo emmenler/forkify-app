c80e624a2a59ba20cd9537ad02ecfb6a70180bdd
Petr Kulikov <p.kulikov.dev@gmail.com>
2023-03-24 12:42:50 +0500

    feat: replace fractional package with fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,6 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
-
+import fracty from 'fracty';
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
   _errorMessage = `We couldn't find this recipe! Please, try another one.`;
@@ -132,7 +131,7 @@ class RecipeView extends View {
           <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ing.quantity ? new Fraction(ing.quantity) : ''
+          ing.quantity ? fracty(ing.quantity).toString() : ''
         }</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
